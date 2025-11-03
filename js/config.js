bf779d1bd480436f742e3e089ec78df28a7cc0ae
chenfeng1212 <marsqq03130618@gmail.com>
2025-11-03 11:09:40 +0800

    帳號回來了

--- js/config.js
+++ js/config.js
@@ -3,9 +3,9 @@ window.CAMP_CONFIG = {
   title: "第28屆工程科學營",
   campStartISO: "2026-02-09T13:00:00+08:00",    // 營隊開始
   applyDeadlineISO: "2025-12-14T23:59:59+08:00", // 報名截止
-  applyFormEmbedURL: "https://docs.google.com/forms/d/e/1FAIpQLSdMIA0sC4FyTTAY8_pyleklkNp_3fmtgVZaXmLs6zedUluJJQ/closedform", // 換成你的內嵌URL
+  applyFormEmbedURL: "https://forms.gle/uqSboLAzN2wxKiBq9", // 換成你的內嵌URL
   facebookURL: "https://www.facebook.com/ESCamp28",
-  contactEmail: "escamp27@gmail.com",
+  contactEmail: "escamp028@gmail.com",
   locationText: "國立成功大學",
   nav: [
     { href: "./index.html",       text: "首頁" },
